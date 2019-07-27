6675a14b247a2c042b8abc25bd705a06dfa5ba76
张陈浩 <chenhao.zhang@2piday.com>
2019-07-27 15:11:42 +0800

    add code

--- src/main/java/com/zifang/springboot/web/controller/SystemController.java
+++ src/main/java/com/zifang/springboot/web/controller/SystemController.java
@@ -3,6 +3,7 @@ package com.zifang.springboot.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SystemController {
@@ -11,4 +12,16 @@ public class SystemController {
     public String index(Model model) {
         return "index";
     }
+
+    @GetMapping(value = {"/main"})
+    public String main(@RequestParam(value = "username") String username,
+                       @RequestParam(value = "password") String password) {
+        if(username.equals("admin")&&password.equals("admin")){
+            return "main";
+        }else{
+            return "error";
+        }
+    }
+
+
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,3 +2,20 @@
 function f() {
     console.log("吃饭吃饭")
 }
+
+function clickButton() {
+    var userName = "aa"
+    var password = "bb"
+    window.location.href='main?username='+userName+"&password="+password;
+    console.log(userName+":"+password)
+}
+
+window.onload=function(){
+    var a  = window.document.getElementById("submit")
+    a.addEventListener("click",clickButton,false)
+
+}
+
+
+
+
